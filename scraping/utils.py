c387c159100764db6703830e1bfa0344fc6e80b4
NoGoodRacing <invisibleborderhc@gmail.com>
2022-11-21 13:24:04 +0300

    add app 'scraping', add 3 tables in app

---
+++ scraping/utils.py
@@ -0,0 +1,43 @@
+cyrillic_letters = {
+    'а': 'a',
+    'б': 'b',
+    'в': 'v',
+    'г': 'g',
+    'д': 'd',
+    'е': 'e',
+    'ё': 'e',
+    'ж': 'zh',
+    'з': 'z',
+    'и': 'i',
+    'й': 'y',
+    'к': 'k',
+    'л': 'l',
+    'м': 'm',
+    'н': 'n',
+    'о': 'o',
+    'п': 'p',
+    'р': 'r',
+    'с': 's',
+    'т': 't',
+    'у': 'u',
+    'ф': 'f',
+    'х': 'h',
+    'ц': 'ts',
+    'ч': 'ch',
+    'ш': 'sh',
+    'щ': 'sch',
+    'ъ': '',
+    'ы': 'y',
+    'ь': '',
+    'э': 'e',
+    'ю': 'yu',
+    'я': 'ya'
+}
+
+
+def from_cyrillic_to_eng(text):
+    text = text.replace(' ', '_').lower()
+    res = ''
+    for i in text:
+        res += cyrillic_letters.get(i, i)
+    return res
