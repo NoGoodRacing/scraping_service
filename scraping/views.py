f0455a1a9f7f954bc8449f7ee7065984df5defe1
NoGoodRacing <invisibleborderhc@gmail.com>
2022-11-23 12:42:44 +0300

    add FindForm, View for home page

--- scraping/views.py
+++ scraping/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import FindForm
+from .models import Vacancy
 
-# Create your views here.
+
+def home_view(request):
+    form = FindForm()
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    qs = []
+    if city or language:
+        filt = {}
+        if city:
+            filt['city__slug'] = city
+        if language:
+            filt['language__slug'] = language
+        qs = Vacancy.objects.filter(**filt)
+    return render(request, 'scraping/home.html', {'object_list': qs, 'form': form})
